e50f2966a0411adde006c281cd17224a11f9b10f
unknown <caleb.davis@CSHELBY-19600.gcu.edu>
2014-06-29 20:03:57 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Ticket Notification App
+
+Add a pane in your Zendesk that notifies you of new tickets in a given view. This app is best used for low volume views (less than 20 active tickets at a time).
+
+### What does this app do?
+
+The ticket notification app will add an app in the top bar location inside your zendesk. When installing the app, you will specify a ticket view that you have already created in your Zendesk. This app will then alert you to any new tickets in that view, and will list the new tickets in that view, the status of the ticket, as well as the ticket requester. 
+
+The app displays a badge that shows the count of new tickets (up to 20) where the app button show in the top bar location. It also plays a sound when a new ticket comes into this view. 
+
+### Customization.
+
+When installing the app, you are asked to provide the ID of the view you want to use this app for. You can find the ID of the view by going to https://yourzendesksubdomain.com/api/v2/views.json. 
+
+You will also be asked to provide a display name for the app. This name will display within the app pane itself. 
+
+If you want to change the sound that plays when a new ticket arrives, you can delete 'alertsound.ogg' in the assets directory, and replace it with your own file. Then, rename your file to 'alertsound'. If you'd like to use a different format than ogg, you must go into the layout.hdbs template, and change the filename there, as the audio element that allows the sound to play is in that part of the app. 
+
+### Disclaimer. 
+
+I take no responsibility if this app in any way harms your Zendesk (though I have no reason to believe it should). It may or may not violate your terms of service with Zendesk, so I would check with that just to be sure. 
+
+The app pulls the view you designate every five seconds. You could modify the app to pull it more frequently than that, but I am not sure if that would clash with the API's rate limiting. Use this app at your own risk! 
+
+Please post if you come up with any issues! I would love to get some feedback on this. 
---
+++ app.js
@@ -0,0 +1,136 @@
+(function() {
+            var on = true;
+    return {
+        events: {
+            // APP Events
+            "app.activated": function(data){
+            if(data.firstLoad){
+            this.popover();
+            }
+            this.requestTickets();
+            },
+            // AJAX Events
+            "getView.done": "requestNames",
+            "getNames.done": "arrayifynames",
+            "goback": _.debounce(function() {
+                if (on === true)
+                this.requestTickets();
+            }, 5000), 
+            // DOM Events
+            "click .alerttoggle": "togglealerts"
+        },
+        requests: {
+        getNames: function(requesters){
+            return{
+                url: '/api/v2/users/show_many.json?ids=' + requesters
+            };
+        },
+        getView: function (){
+            var view = this.setting('view_id');
+            return {
+            url: '/api/v2/views/' + view + '/tickets.json',
+            type: 'GET'
+            };
+        }
+    },
+    names: function(requesters){
+        this.ajax('getNames', requesters);
+    },
+    togglealerts: function(){
+        on = this.store('togglealerts');
+        this.$('.alerttoggle').toggleClass('on');
+        if (on === null){
+            on = true;
+        }
+        on = !on;
+        if (on === false){
+        this.changeicons("off");    
+        this.switchTo('off');
+        }
+        if (on === true){
+        this.switchTo('spinner');}
+        this.store({togglealerts: on});
+        this.trigger('goback');
+    },
+    requestTickets: function(){
+        var currentuser = this.currentUser();
+        var name = currentuser.name();
+        var nameArr = name.split(" ");
+        this.store({name: nameArr[0]});
+        var role = currentuser.role();
+        if (role == 533295 || on === false) {
+            return false;
+        }
+        this.ajax('getView');
+    },
+    requestNames: function(data){
+        var requestersjson = [];
+        for (var i = 0; i < data.count; i++){
+            requestersjson.push(data.tickets[i].requester_id);
+        }
+        this.store({tickets: data});
+        var requesters = requestersjson.join();
+        this.ajax('getNames', requesters);
+    },
+    arrayifynames: function(data){
+        var names = {};
+        for (var i = 0; i < data.count; i++){
+            names[data.users[i].id] = data.users[i].name;
+        }
+            this.iteratetickets(names);
+    },
+    iteratetickets: function(name){
+        var names = name;
+        var tickets = {"tickets":[]};
+        var data = this.store('tickets');
+        var length = data.tickets.length;
+        for (var i = 0; i < data.count; i++){
+            var temp = {
+                id: data.tickets[i].id,
+                description: data.tickets[i].description,
+                status: data.tickets[i].status,
+                url: "/agent/#/tickets/" + data.tickets[i].id,
+                subject: data.tickets[i].subject,
+                name: names[data.tickets[i].requester_id]
+            };
+            tickets.tickets.push(temp);
+        }
+
+        this.displaynotifications(tickets, length, data);
+    },
+    displaynotifications: function(tickets, length){
+        var audiocheck = this.$('audio').length;
+        this.switchTo('list', tickets);
+        this.changeicons("on", length);
+        var listedtickets = this.store('listedtickets');
+        if (listedtickets == null){
+            this.store({listedtickets: tickets});
+            listedtickets = this.store('listedtickets');
+        }
+        var newtickets = _.pluck(tickets.tickets, 'id');
+        var listed = _.pluck(listedtickets.tickets, 'id');
+        var newticket = _.difference(newtickets, listed);
+        var count = newticket.length;
+        if (count > 0){
+         this.popover();
+            if (audiocheck > 0){
+                this.$('.alertsound').get(0).play();}
+            
+        }
+        this.store({listedtickets: tickets});
+        this.trigger('goback');
+        },
+    changeicons: function(tog, length){
+        if (tog == "on" && length > 0){
+        this.setIconState('active', this.assetURL('Ticket-Tally_' + length + '.png'));
+        this.setIconState('inactive', this.assetURL('Ticket-Tally_' + length + '.png'));
+        this.setIconState('hover', this.assetURL('Ticket-Tally_' + length + '.png'));
+    }
+    else {
+        this.setIconState('active', this.assetURL('icon_top_bar_inactive.png'));
+        this.setIconState('inactive', this.assetURL('icon_top_bar_inactive.png'));
+        this.setIconState('hover', this.assetURL('icon_top_bar_inactive.png'));
+        }
+    }
+    };
+})();
